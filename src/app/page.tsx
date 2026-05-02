5c84998284632d507fadc6569585682d9d25c503
Mitya <mitywaflow@gmail.com>
2026-05-03 01:31:54 +0200

    update
    Signed-off-by: Mitya <mitywaflow@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "../components/organisms/Hero/Hero";
-
+import NavigatingSec from "../components/organisms/Sections/Navigating/NavigatingSec";
 export default function Home() {
   return (
     <>
       <Hero />
+      <NavigatingSec />
     </>
   );
 }
---
+++ src/components/organisms/Sections/Navigating/NavigatingSec.tsx
@@ -0,0 +1,11 @@
+import FirstBanner from "@/src/components/molecules/Banners/FirstBanner";
+
+export default function NavigatingSec() {
+  return (
+    <>
+      <div>
+        <FirstBanner />
+      </div>
+    </>
+  );
+}

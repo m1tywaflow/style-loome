9da53e6dd82d52dd4f6b53c0fd748bee5cbf129b
Mitya <mitywaflow@gmail.com>
2026-05-25 15:38:44 +0200

    update
    Signed-off-by: Mitya <mitywaflow@gmail.com>

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -1,8 +1,14 @@
 import Explore from "@/src/components/organisms/products/Explore/Explore";
+import Comments from "@/src/components/organisms/Sections/CommentsSection/Comments";
+import FAQ from "@/src/components/organisms/Sections/FAQ/FAQ";
+import YellowBanner from "@/src/components/molecules/Banners/YellowBanner";
 export default function Products() {
   return (
     <>
       <Explore />
+      <Comments />
+      <FAQ />
+      <YellowBanner />
     </>
   );
 }
